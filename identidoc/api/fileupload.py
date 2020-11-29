d65a653b6cef0896e07883f9ec5df82398a686ef
Jonathan Marek <jonathan.marek@mavs.uta.edu>
2020-11-29 17:22:34 -0600

    Ensure secure filename

--- identidoc/api/fileupload.py
+++ identidoc/api/fileupload.py
@@ -1,8 +1,8 @@
 # RESTful API Resource for file uploads
 
 import os
-
 from flask_restful import Resource, request
+from werkzeug.utils import secure_filename
 
 # List of allowed file extensions
 file_extensions=['PDF','PNG','JPG','JPEG','TXT']
@@ -15,7 +15,8 @@ if not os.path.exists(UPLOAD_PATH):
 class FileUpload(Resource):
     def post(self):
         file = request.files['FILE_NAME']
-        filename = file.filename
+        # TODO: Append a timestamp as a unique identifier to the filename
+        filename = secure_filename(file.filename)
 
         if '.' in filename and filename.rsplit('.', 1)[1].upper() in file_extensions:
             file.save(os.path.join(UPLOAD_PATH, filename))
