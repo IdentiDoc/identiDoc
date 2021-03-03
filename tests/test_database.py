162c5cee651946af73b965e2b24c0fcfebd81f03
Jonathan Marek <jonathan.marek@mavs.uta.edu>
2021-03-03 12:38:14 -0600

    Unit Test did not reflect reality, now it does

--- tests/test_database.py
+++ tests/test_database.py
@@ -417,7 +417,7 @@ class TestDB(unittest.TestCase):
     @staticmethod
     def is_json(myjson):
         try:
-            json_object = json.loads(json.dump(myjson))
+            json_object = json.loads(json.dumps(myjson))
         except ValueError as e:
             return False
         return True
