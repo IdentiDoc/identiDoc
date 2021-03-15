ed2025d3db67b1aa580f7bff626a85af8f2ef166
Jonathan Marek <jonathan.marek@mavs.uta.edu>
2021-03-15 10:59:09 -0500

    Added another env variable

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,6 +1,7 @@
 # This unit test ensures that the identidoc environment is correctly set up.
 # If this unit test fails, the current version of identiDoc should not be deployed.
 
+from identidoc.services.signaturedetection import signature_detection
 import unittest
 import os
 
@@ -11,8 +12,10 @@ class TestEnv(unittest.TestCase):
         temp_path = os.environ.get('IDENTIDOC_TEMP_PATH', 'ERROR')
         db_path = os.environ.get('IDENTIDOC_DB', 'ERROR')
         classification_models = os.environ.get('IDENTIDOC_CLASSIFICATION_MODELS', 'ERROR')
+        signature_detection - os.environ.get('IDENTIDOC_SIGNATURE_DETECTION', 'ERROR')
 
         assert upload_path != 'ERROR'
         assert temp_path != 'ERROR'
         assert db_path != 'ERROR'
         assert classification_models != 'ERROR'
+        assert signature_detection != 'ERROR'
