565f37fda9e3f4d834ed997e5ae00302349978c6
Jonathan Marek <jonathan.marek@mavs.uta.edu>
2021-03-18 17:43:37 -0500

    No idea why VS Code added that...

--- identidoc/api/fileupload.py
+++ identidoc/api/fileupload.py
@@ -2,7 +2,6 @@
 
 import os
 from flask_restful import Resource, request
-from numpy import sign
 from werkzeug.utils import secure_filename
 
 from identidoc.services import get_current_time_as_POSIX_timestamp, process_uploaded_file
@@ -31,7 +30,7 @@ class FileUpload(Resource):
 
             if document_classification is None or signature_presense is None:
                 return { 'message' : 'Database Error. Classification not recorded.' }, 400
-                
+
             if document_classification == 0:
                 # Don't run a signature detection on unrecognized documents
                 signature_presense = 'NONE'
