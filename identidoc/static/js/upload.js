ea5a1910675e58dd83e59bce8bade3400b03d7ef
Jonathan Marek <jonathan.marek@mavs.uta.edu>
2021-04-06 19:53:56 -0500

    Let's not worry about heic right now

--- identidoc/static/js/upload.js
+++ identidoc/static/js/upload.js
@@ -72,34 +72,17 @@ function loadFile(event) {
   var filename = selectedFile.name;
   var fileext = filename.split('.').pop();
 
-  if(fileext == 'heic') {
-    // For heic, convert the file
-    fetch(URL.createObjectURL(selectedFile))
-      .then((res) => res.blob)
-      .then((blob) => heic2any({ 
-        blob,
-        toType: "image/jpeg",
-        quality: 0.5,
-       }))
-       .then((conversionResult) => {
-         alert(conversionResult);
-       })
-       .catch((e) => {
-         alert(e);
-       });
+  if (fileext == 'heic') {
 
   } else if (fileext == 'pdf') {
 
   } else {
+    output.style.visibility = 'visible';
+    output.src = URL.createObjectURL(selectedFile);
+    output.onload = function () {
+      URL.revokeObjectURL(output.src) // free memory
+    }
 
-  }
-
-
-
-  output.style.visibility = 'visible';
-  output.src = URL.createObjectURL(selectedFile);
-  output.onload = function () {
-    URL.revokeObjectURL(output.src) // free memory
   }
 }
 
