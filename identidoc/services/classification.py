d0268ab66be2bef6e83432e8a7ae67cbb899ffbf
Jonathan Marek <jonathan.marek@mavs.uta.edu>
2021-04-16 10:00:32 -0500

    Reformat some spacing

--- identidoc/services/classification.py
+++ identidoc/services/classification.py
@@ -8,8 +8,10 @@ CLASSIFICATION_MODELS_PATH = os.environ['IDENTIDOC_CLASSIFICATION_MODELS']
 # Top level function that returns the classification of the document
 # The text of the document is passed in as a singular string and
 # the function returns the classification of the uploaded document as an int.
+
+
 def predict_document_class(text):
-    #more preprocessing
+    # more preprocessing
 
     vectorized_data = vectorizer(text)
     classification = classifier(vectorized_data)
@@ -24,17 +26,18 @@ def vectorizer(extracted_text):
 
 
 def classifier(vectorized_data):
-    prediction_threshold=0.8
-    loaded_model = pickle.load(open(os.path.join(CLASSIFICATION_MODELS_PATH, "model.pkl"), "rb"))
+    prediction_threshold = 0.8
+    loaded_model = pickle.load(
+        open(os.path.join(CLASSIFICATION_MODELS_PATH, "model.pkl"), "rb"))
     prediction = loaded_model.predict_proba(vectorized_data)
-    predicted =prediction[0]
-    selected_class=0
-    max_prob=0
+    predicted = prediction[0]
+    selected_class = 0
+    max_prob = 0
 
-    for i,predictions in enumerate(predicted):
-        if predictions>prediction_threshold:
-            if predictions>max_prob:
-                max_prob=predictions
-                selected_class=i
+    for i, predictions in enumerate(predicted):
+        if predictions > prediction_threshold:
+            if predictions > max_prob:
+                max_prob = predictions
+                selected_class = i
 
     return selected_class
